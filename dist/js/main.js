d5a07c0205c5850b61ecc4b12ef73d08707c5349
gajanan1992 <khatkegajanan@gmail.com>
2018-09-13 13:17:00 +0530

    menu Transation added and potrait added

---
+++ dist/js/main.js
@@ -0,0 +1,38 @@
+
+//get dom element
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItem = document.querySelectorAll(".nav-item");
+
+let showMenu = false;
+menuBtn.addEventListener('click',toggleMenu);
+
+function toggleMenu(){
+    
+    if(!showMenu){
+        menuBtn.classList.add("close");
+        menu.classList.add("show");
+        menuNav.classList.add("show");
+        menuBranding.classList.add("show");
+        navItem.forEach(element => {
+            element.classList.add("show");
+        });
+// menu state
+        showMenu = true;
+    }else {
+        menuBtn.classList.remove("close");
+
+        menu.classList.remove("show");
+        menuNav.classList.remove("show");
+        menuBranding.classList.remove("show");
+        navItem.forEach(element => {
+            element.classList.remove("show");
+        });
+        //menu state
+        showMenu = false;
+    }
+    
+}
+
